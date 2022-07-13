8e57838ed22abd1c49a1f66b72261ea73386d379
JamieT966 <jamietarpey1@gmail.com>
2022-07-13 15:49:26 +0000

    Added basic booking model and created view in admin site

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import datetime, date
 
-# Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=10)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False, blank=True)
+    selected_date = models.DateTimeField("Selected Date: mm/dd/yyyy", auto_now_add=False, auto_now=False, blank=True, null=True)
+
+    def __str__(self):
+        return self.selected_date
