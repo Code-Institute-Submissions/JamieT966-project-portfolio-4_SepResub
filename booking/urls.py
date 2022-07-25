d086b67b881a286392760535dbf0bcf901077f70
JamieT966 <jamietarpey1@gmail.com>
2022-07-25 12:06:03 +0000

    added links to buttons on booking page

--- booking/urls.py
+++ booking/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.BookingPageView, name='booking_home'),
     path('booking_form/', views.BookingForm, name='booking'),
-    path('my_booking/', views.MyBooking, name='my_booking'),
+    path('my-booking/', views.MyBooking, name='my_booking'),
 ]
