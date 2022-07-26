632cc610882e63de46454cb876d6db875d5894c1
JamieT966 <jamietarpey1@gmail.com>
2022-07-26 13:41:11 +0000

    added comments

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,6 @@
 console.log('hello')
 
+// Function to make alerts disappear automatically
 setTimeout(function () {
     let messages = document.getElementById('msg');
     let alert = new bootstrap.Alert(messages);
