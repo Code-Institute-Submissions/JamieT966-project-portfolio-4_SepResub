b963a59779314825f2ce85a64f2ea480d4a0c652
JamieT966 <jamietarpey1@gmail.com>
2022-07-26 18:38:39 +0000

    deploy to heroku

--- modern_landscapes/settings.py
+++ modern_landscapes/settings.py
@@ -152,10 +152,8 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # E-mail notifications for both Development and Production
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.environ.get('modernlandscapesgardens@gmail.com')
-EMAIL_HOST_PASSWORD = os.environ.get('pfmxxeimlpojjdfw')
-DEFAULT_FROM_EMAIL = os.environ.get('modernlandscapesgardens@gmail.com')
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
