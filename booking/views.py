896cc3e048c23c6576b411b0895fe16a1925d59f
JamieT966 <jamietarpey1@gmail.com>
2022-07-28 08:26:33 +0000

    pushing to heroku to test email

--- booking/views.py
+++ booking/views.py
@@ -34,7 +34,7 @@ def BookingForm(request):
             #     'modernlandscapesgardens@gmail.com',
             #     [f'{email}']
             # )
-            template = render_to_string('email_template.html',{name: form.cleaned_data['name']})
+            template = render_to_string('email_template.html',{name: booking.name})
             mail = EmailMessage(
                 'Booking Confirmation',
                 template,
