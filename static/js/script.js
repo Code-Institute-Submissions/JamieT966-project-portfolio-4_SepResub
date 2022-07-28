ad2b04374a11698dc6d9e27303d594790fcf45f7
JamieT966 <jamietarpey1@gmail.com>
2022-07-28 11:05:28 +0000

    more comments added

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,12 @@
 // Function to make alerts disappear automatically
+
 setTimeout(function () {
     let messages = document.getElementById('msg');
     let alert = new bootstrap.Alert(messages);
     alert.close();
 }, 4000);
 
-
+// Bootstrap modal code
 $('#delete-modal').on('shown.bs.modal', function () {
     $('#reference_match').trigger('focus')
   })
