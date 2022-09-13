f387ee8ad226f67292d9166920d8786d5598a632
JamieT966 <jamietarpey1@gmail.com>
2022-09-13 07:51:34 +0000

    final deployment

--- modern_landscapes/settings.py
+++ modern_landscapes/settings.py
@@ -105,16 +105,16 @@ WSGI_APPLICATION = 'modern_landscapes.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 # TO TEST I COMMENTED OUT POSTGRES AND UNCOMMENTED SQL DATABASE
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
